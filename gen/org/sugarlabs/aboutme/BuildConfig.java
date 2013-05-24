d101afee9287db50d435db923feb14cf7f4c704c
Manuel Quiñones <manuq@laptop.org>
2013-05-24 00:40:19 -0300

    Initial commit

---
+++ gen/org/sugarlabs/aboutme/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.sugarlabs.aboutme;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/org/sugarlabs/aboutme/AboutMeActivity.java
@@ -0,0 +1,23 @@
+package org.sugarlabs.aboutme;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AboutMeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.about_me, menu);
+        return true;
+    }
+    
+}
