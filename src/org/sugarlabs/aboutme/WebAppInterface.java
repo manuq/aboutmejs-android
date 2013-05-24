1f09776ba0e5be490e215ac37748f44f9e6ff3ed
Manuel Quiñones <manuq@laptop.org>
2013-05-24 01:28:33 -0300

    Add files forgotten in previous commit

---
+++ src/org/sugarlabs/aboutme/WebAppInterface.java
@@ -0,0 +1,26 @@
+package org.sugarlabs.aboutme;
+
+import android.app.Activity;
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+
+public class WebAppInterface {
+    Context mContext;
+
+    /** Instantiate the interface and set the context */
+    WebAppInterface(Context c) {
+        mContext = c;
+    }
+
+    @JavascriptInterface
+    public void stop() {
+    	// Maybe is better to stop it, not finish it
+    	((Activity) mContext).finish();
+    }
+
+    @JavascriptInterface
+    public String getXOColor() {
+        // I don't know how to use arrays in java yet :)
+        return "#FFFF00,#00FFFF";
+    }
+}
